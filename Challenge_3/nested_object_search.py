442346a4d9f51728997ac2be55c7faa53ead2b29
Chandrashekhar Rachakonda <shekhar.rachakonda@gmail.com>
2022-10-31 10:34:19 +0530

    Added hadler for non-searchable boject

--- Challenge_3/nested_object_search.py
+++ Challenge_3/nested_object_search.py
@@ -8,10 +8,15 @@ def get_value(obj: dict, key:str)-> Optional[str]:
     try:
         keys = key.split('/')
         for k in keys:
+            if type(k) != dict:
+                raise TypeError()
             value = value[k]
     except KeyError:
         print("Invalid Key", k)
         return None # Incase of key error returns None
+    except TypeError:
+        print("the object is not longer searchable")
+        return None # If key is too long and object is no longer searchable
     return value
 
 
